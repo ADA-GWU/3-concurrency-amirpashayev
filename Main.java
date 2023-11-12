ded95ac268ac00c0f9bbf74cadf9bf70c764798d
amirpashayev <58549204+amirpashayev@users.noreply.github.com>
2023-11-12 21:11:31 +0400

    Concurrency Task

---
+++ Main.java
@@ -0,0 +1,153 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class Main extends JFrame
+{
+    private BufferedImage image;
+    private BufferedImage resultImage;
+    private int squareSize;
+    private Graphics2D graphics;
+
+
+    public Main(String title) throws HeadlessException
+    {
+        super(title);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    private void updateUI()
+    {
+        SwingUtilities.invokeLater(this::repaint);
+    }
+
+    private Color getAverageColor(int startX, int startY)
+    {
+        int red = 0, green = 0, blue = 0;
+        int count = 0;
+
+        for (int x = startX; x < startX + this.squareSize; x++) {
+            for (int y = startY; y < startY + this.squareSize; y++) {
+                if (x < this.image.getWidth() && y < this.image.getHeight()) {
+                    Color pixelColor = new Color(this.image.getRGB(x, y));
+                    red += pixelColor.getRed();
+                    green += pixelColor.getGreen();
+                    blue += pixelColor.getBlue();
+                    count++;
+                }
+            }
+        }
+
+        return new Color(red / count, green / count, blue / count);
+    }
+
+    private void colorPixelsInsideSquare(Color pixelColor, int startX, int startY)
+    {
+        for (int x = startX; x < startX + this.squareSize; x++) {
+            for (int y = startY; y < startY + this.squareSize; y++) {
+                if (x < this.resultImage.getWidth() && y < this.resultImage.getHeight()) {
+                    this.resultImage.setRGB(x, y, pixelColor.getRGB());
+                }
+            }
+        }
+    }
+
+    private void processSquare(int startX, int startY)
+    {
+        Color averageColor = this.getAverageColor(startX, startY);
+        this.colorPixelsInsideSquare(averageColor, startX, startY);
+
+        updateUI();
+        try {
+            Thread.sleep(5);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void initImage(String filename) throws IOException
+    {
+        this.image = ImageIO.read(new File(filename));
+        this.resultImage = new BufferedImage(this.image.getWidth(), this.image.getHeight(), BufferedImage.TYPE_INT_RGB);
+        this.graphics = resultImage.createGraphics();
+        this.graphics.drawImage(this.image, 0, 0, this);
+        setSize(this.image.getWidth(), this.image.getHeight());
+        setVisible(true);
+    }
+
+    private void executeMultiThread()
+    {
+        int cores = Runtime.getRuntime().availableProcessors();
+        int totalRows = image.getHeight();
+        int rowsPerThread = totalRows / cores;
+        ExecutorService executor = Executors.newFixedThreadPool(cores);
+
+        for (int i = 0; i < cores; i++) {
+            final int startRow = i * rowsPerThread;
+            final int endRow = (i == cores - 1) ? totalRows : (startRow + rowsPerThread);
+
+            executor.submit(() -> this.processSquaresInImage(startRow, endRow));
+        }
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    private void processSquaresInImage(int startRow, int endRow)
+    {
+        for (int y = startRow; y < endRow; y += this.squareSize) {
+            for (int x = 0; x < this.image.getWidth(); x += this.squareSize) {
+                processSquare(x, y);
+            }
+        }
+    }
+
+
+    public void processImage(String filename, int squareSize, char mode) throws Exception
+    {
+        this.squareSize = squareSize;
+        this.initImage(filename);
+
+        if (mode == 'S') {
+            this.processSquaresInImage(0, this.image.getHeight());
+        } else if (mode == 'M') {
+            this.executeMultiThread();
+        }
+
+        graphics.dispose();
+        ImageIO.write(resultImage, "jpg", new File("result.jpg"));
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        if (resultImage != null) {
+            g.drawImage(resultImage, 0, 0, this);
+        } else {
+            super.paint(g);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 3) {
+            System.out.println("Usage: java Main.java <filename> <squareSize> <S|M>");
+            return;
+        }
+        String filename = args[0];
+        int squareSize = Integer.parseInt(args[1]);
+        char mode = args[2].charAt(0);
+        Main processor = new Main("Image Processor");
+        processor.processImage(filename, squareSize, mode);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/YybNWfh8)
+
+# Concurrency
+
+
+## About the project
+
+This assignment focuses on implementing concurrency on a picture by applying interpolation to each row that consists of a square of pixels, size of which is given as an argument when the program is run. When the program is run in a single thread mode (also passed as an argument from terminal/command prompt) the rows of the picture are analysed one by one. However, in multi-thread mode, the rows of the picture are analysed concurrently (the number of rows analysed at the same time depends on the number of cores of the computer). The result is then saved to a different file - result.jpg.
+
+
+## Dependencies and instructions to run
+
+You should have JDK 15 or later installed on your computer. When running the Main.java file it should be in the following style: "java {path to the Main.java file} {path to the jpg file} {square size} {Single or Multi-Thread (S/M)}".
+
